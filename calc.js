171934b6c478f78d23fa454a607902dd26b3e61b
thiago <tchiqueti@gmail.com>
2023-05-09 17:18:49 -0300

    Alteração do método focus quando apaga o calculo utlizando c

--- calc.js
+++ calc.js
@@ -14,7 +14,6 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
 
 document.getElementById('clear').addEventListener('click', function (){
   input.value = ''
-  input.focus()
 })
 
 document.getElementById('equal').addEventListener('click', calculate)
